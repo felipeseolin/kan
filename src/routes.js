1440f4423c2bf169b613a0fb0d94d69900b723d6
felipeseolin <seolin09>
2019-10-22 01:30:21 -0300

    New routes

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,19 @@ const CardController = require('./controllers/CardController');
 
 routes.get('/', (req, res) => res.render('index', { title: 'Login' }));
 
+// Lists
 routes.get('/lists', ListController.index);
+routes.get('/lists/new', ListController.form);
+routes.post('/lists/new', ListController.store);
 routes.get('/lists/:id', ListController.show);
-routes.post('/lists', ListController.store);
 routes.patch('/lists/:id', ListController.update);
 routes.get('/lists/destroy/:id', ListController.destroy);
-
-routes.post('/cards', CardController.store);
+// Cards
+routes.get('/cards', CardController.index);
+routes.get('/cards/new', CardController.form);
+routes.post('/cards/new', CardController.store);
+routes.get('/cards/:id', CardController.show);
+routes.patch('/cards/:id', CardController.update);
+routes.get('/cards/destroy/:id', CardController.destroy);
 
 module.exports = routes;
