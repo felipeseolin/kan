002120465a072f8459d8aca7cf8c98c8f0d31f45
rafaelfmar <rafael.fmarques@outlook.com>
2019-10-28 16:31:30 -0300

    Password hash correction

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Bcrypt = require('bcrypt');
 
 const UserInit = require('../models/User');
 const BoardInit = require('../models/Board');
@@ -12,6 +13,7 @@ const Card = mongoose.model('Card');
 
 module.exports = {
   async store(req, res) {
+    req.body.password = Bcrypt.hashSync(req.body.password, 10);
     const user = await User.create(req.body);
     if (!user) {
       return res.render('errror');
--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
-const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema(
   {
@@ -15,9 +13,10 @@ const UserSchema = new Schema(
       required: true,
       unique: true,
     },
-    hashed_password: {
+    password: {
       type: String,
       default: '',
+      required: true,
     },
     boards: [
       {
@@ -28,28 +27,5 @@ const UserSchema = new Schema(
   },
   { timestamps: true }
 );
-
-UserSchema.virtual('password').set(password => {
-  this._password = password;
-});
-
-UserSchema.pre('save', next => {
-  const user = this;
-  if (user._password === undefined) {
-    return next();
-  }
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) {
-      console.log(err);
-    }
-    bcrypt.hash(user._password, salt, (error, hash) => {
-      if (err) {
-        console.log(error);
-      }
-      user.hashed_password = hash;
-      next();
-    });
-  });
-});
 
 mongoose.model('User', UserSchema);
