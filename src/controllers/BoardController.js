11bbe87dea13ceb86cc2b27f1c8b67ab92132aa9
felipeseolin <seolin09>
2019-10-28 22:28:31 -0300

    Board Validation.

--- src/controllers/BoardController.js
+++ src/controllers/BoardController.js
@@ -8,6 +8,25 @@ const Board = mongoose.model('Board');
 const List = mongoose.model('List');
 const Card = mongoose.model('Card');
 
+function validate(name, description) {
+  let errors = [];
+
+  if (!name || name.trim().length === 0) {
+    errors = [...errors, 'Um nome deve ser dado ao quadro.'];
+  }
+  if (name && name.length > 100) {
+    errors = [...errors, 'O nome deve ter ao máximo 100 caracteres.'];
+  }
+  if (name && name.length < 5) {
+    errors = [...errors, 'O nome deve ter ao mínimo 5 caracteres.'];
+  }
+  if (description && description.length > 250) {
+    errors = [...errors, 'A descrição deve ter ao máximo 250 caracteres.'];
+  }
+
+  return errors;
+}
+
 module.exports = {
   async index(req, res) {
     const boards = await Board.find();
@@ -23,10 +42,18 @@ module.exports = {
     });
   },
   async store(req, res) {
-    const list = await Board.create(req.body);
-
-    if (!list) {
-      return res.render('errror');
+    // Validation
+    const errors = validate(req.body.name, req.body.description);
+    if (errors.length > 0) {
+      req.flash('error', errors);
+      return res.redirect('/boards/new');
+    }
+    // Create Board
+    const board = await Board.create(req.body);
+    // Verify if the board is saved
+    if (!board) {
+      req.flash('error', ['Ocorreu um erro ao salvar o quadro, tente novamente.']);
+      return res.redirect('/boards/new');
     }
 
     return res.redirect('/boards');
@@ -35,15 +62,27 @@ module.exports = {
     const board = await Board.findById(req.params.id);
 
     return res.render('board.create.handlebars', {
-      title: `Editar Quadro: ${board.name}`,
+      title: `Editar: ${board.name}`,
       formAction: `/boards/edit/${board.id}`,
       board,
     });
   },
   async update(req, res) {
+    // Validation
+    const errors = validate(req.body.name, req.body.description);
+    if (errors.length > 0) {
+      req.flash('error', errors);
+      return res.redirect(`/boards/edit/${req.params.id}`);
+    }
+    // Update Board
     const board = await Board.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
+    // Verify if the board is saved
+    if (!board) {
+      req.flash('error', ['Ocorreu um erro ao salvar o quadro, tente novamente.']);
+      return res.redirect('/boards/new');
+    }
 
     return res.redirect('/boards');
   },
