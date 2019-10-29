20ca2402aab250a262dbee4b8ca89ad0fcb01a8c
felipeseolin <seolin09>
2019-10-28 21:26:06 -0300

    Added flash configuration

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const hbs = require('express-handlebars');
+const flash = require('express-flash');
+const session = require('express-session');
 
 const app = express();
 app.use(express.json());
@@ -10,6 +12,14 @@ app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, './src/views'));
 app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
+
+app.use(session({
+  secret: 'b7c673bc498b874cb6a0e43ef9b86919',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(flash());
 
 mongoose.connect('mongodb://localhost:27017/kan', {
   useNewUrlParser: true,
